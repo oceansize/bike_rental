6d7dcb6d758b1fc0ab1b22def2f37ac3831f0c11
Roi Driscoll <roi.driscoll@gmail.com>
2014-08-14 21:54:31 +0100

    Docking Station able to set default capacity on initialisation

---
+++ spec/bike_container_spec.rb
@@ -0,0 +1,46 @@
+require 'bike_container'
+
+class ContainerHolder; include BikeContainer; end
+
+describe BikeContainer do
+
+  let(:bike)    { Bike.new            }
+  let(:holder)  { ContainerHolder.new }
+
+  def fill_holder holder
+    10.times { holder.store(Bike.new) }
+  end
+
+  it 'should accept a bike' do
+    expect(holder.bike_count).to eq 0
+    holder.store(bike)
+    expect(holder.bike_count).to eq 1
+  end
+
+  it 'should release a bike' do
+    holder.store(bike)
+    holder.release(bike)
+    expect(holder.bike_count).to eq 0
+  end
+
+  it 'should know when it is full' do
+    expect(holder).not_to be_full
+    fill_holder holder
+    # expect(holder).to be_full
+  end
+
+  it 'should not accept a bike if full' do
+    fill_holder holder
+    expect(lambda { holder.store(bike) }).to raise_error(RuntimeError)
+  end
+
+  it 'should provide a list of available bikes' do
+    working_bike, broken_bike = Bike.new, Bike.new
+    broken_bike.break!
+    holder.store working_bike
+    holder.store broken_bike
+    expect(holder.available_bikes).to eq([working_bike])
+  end
+
+
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,42 +2,10 @@ require './lib/docking_station'
 
 describe DockingStation do
 
-  let(:bike)    { Bike.new                            }
-  let(:station) { DockingStation.new(:capacity => 20) }
+  let(:station) { DockingStation.new(:capacity => 999) }
 
-  def fill_station station
-    20.times { station.store(Bike.new) }
-  end
-
-  it 'should accept a new bike' do
-    expect(station.bike_count).to eq 0
-    station.store(bike)
-    expect(station.bike_count).to eq 1
-  end
-
-  it 'should release a bike' do
-    station.store(bike)
-    station.release(bike)
-    expect(station.bike_count).to eq 0
-  end
-
-  it 'should know when it is full' do
-    expect(station).not_to be_full
-    fill_station station
-    expect(station).to be_full
-  end
-
-  it 'should not accept a bike if full' do
-    fill_station station
-    expect(lambda { station.store(bike) }).to raise_error(RuntimeError)
-  end
-
-  it 'should provide a list of available bikes' do
-    working_bike, broken_bike = Bike.new, Bike.new
-    broken_bike.break!
-    station.store working_bike
-    station.store broken_bike
-    expect(station.available_bikes).to eq([working_bike])
+  it 'should allow setting default capacity on initialising' do
+    expect(station.capacity).to eq 999
   end
 
 end
